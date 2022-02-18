37d603f0538e010cd1f880c9d1b22766f91ddc4c
MikolajMroczkowski <mikolaj.mroczkowski@gmail.com>
2022-02-18 20:35:52 +0100

    update sql querys

--- zapytanie.sql
+++ zapytanie.sql
@@ -1,43 +1,35 @@
-SELECT IF(IFNULL((SELECT COUNT(*)
-                  FROM daneDni
-                           INNER JOIN typyDni on typyDni.id = daneDni.typeDay
-                           INNER JOIN maxValsGroups on typyDni.maxValGroup = maxValsGroups.id
-                  WHERE maxValsGroups.id =
-                        (SELECT maxValsGroups.id
-                         FROM maxValsGroups
-                                  INNER JOIN typyDni on typyDni.maxValGroup = maxValsGroups.id
-                         WHERE typyDni.id = 2
-                           AND maxValsGroups.userGroup = 3)
-                    AND date = '2022-12-01'
-                  GROUP BY maxValsGroups.id), 0) <
-          IFNULL((SELECT maxValsGroups.val
-                  FROM maxValsGroups
-                           INNER JOIN typyDni on typyDni.maxValGroup = maxValsGroups.id
-                  WHERE typyDni.id = 2
-                    AND maxValsGroups.userGroup = 3), IFNULL((SELECT COUNT(*)
-                                                              FROM daneDni
-                                                                       INNER JOIN typyDni on typyDni.id = daneDni.typeDay
-                                                                       INNER JOIN maxValsGroups on typyDni.maxValGroup = maxValsGroups.id
-                                                              WHERE maxValsGroups.id =
-                                                                    (SELECT maxValsGroups.id
-                                                                     FROM maxValsGroups
-                                                                              INNER JOIN typyDni on typyDni.maxValGroup = maxValsGroups.id
-                                                                     WHERE typyDni.id = 2
-                                                                       AND maxValsGroups.userGroup = 3)
-                                                                AND date = '2022-12-01'
-                                                              GROUP BY maxValsGroups.id), 0) + 1) AND
-          IFNULL((SELECT COUNT(typeDay)
-                  FROM daneDni
-                           INNER JOIN users on daneDni.user = users.id
-                  WHERE typeDay = 2
-                    AND date = '2022-12-01'
-                    AND users.grupaZawodowa = 3
-                  GROUP BY typeDay), 0) <
-          IFNULL((SELECT val FROM maxVal WHERE type = 2 AND userGroup = 3), IFNULL((SELECT COUNT(typeDay)
-                                                                                    FROM daneDni
-                                                                                             INNER JOIN users on daneDni.user = users.id
-                                                                                    WHERE typeDay = 2
-                                                                                      AND date = '2022-12-01'
-                                                                                      AND users.grupaZawodowa = 3
-                                                                                    GROUP BY typeDay), 0) + 1)
-           , 'true', 'false') as canWork
+# get all users who used type
+SELECT users.name AS firstName, users.surname AS LastName
+FROM daneDni
+         INNER JOIN users ON users.id = daneDni.user
+         INNER JOIN maxVal ON users.grupaZawodowa = maxVal.userGroup AND daneDni.typeDay = maxVal.type
+WHERE date = '2021-12-02'
+  AND daneDni.typeDay = 1
+  AND users.grupaZawodowa = 3;
+
+# get max value for type
+SELECT IFNULL((SELECT val
+               FROM maxVal
+               WHERE userGroup = 3
+                 AND type = 1), -1) val;
+
+# get all used who used group
+SELECT users.name AS firstName, users.surname AS LastName
+FROM daneDni
+         INNER JOIN typyDni ON typyDni.id = daneDni.typeDay
+         INNER JOIN users ON users.id = daneDni.user
+         INNER JOIN maxValsGroups
+                    ON maxValsGroups.id = typyDni.maxValGroup AND users.grupaZawodowa = maxValsGroups.userGroup
+WHERE date = '2021-12-01'
+  AND users.grupaZawodowa = 3
+  AND (SELECT maxValGroup
+       FROM typyDni
+                INNER JOIN maxValsGroups ON typyDni.maxValGroup = maxValsGroups.id AND maxValsGroups.userGroup = 3
+       WHERE typyDni.id = 1) = maxValsGroups.id;
+
+# get max group value
+SELECT IFNULL((SELECT maxValsGroups.val
+FROM typyDni
+         INNER JOIN maxValsGroups ON typyDni.maxValGroup = maxValsGroups.id
+WHERE typyDni.id = 1
+  AND maxValsGroups.userGroup = 3),-1) val;
